c487d33badcde38dc91f1123c9f0f3fa5db5c583
Carlos Ballena <687315+cballenar@users.noreply.github.com>
2024-04-02 17:29:23 -0400

    Enables lazyvim default plugins

--- nvim/.config/nvim/lua/cballenar/plugins/plugins.lua
+++ nvim/.config/nvim/lua/cballenar/plugins/plugins.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+-- if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -9,6 +9,8 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  -- Default Example LazyVim Plugins
+  -- --------------------------------------------------------------------------
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
